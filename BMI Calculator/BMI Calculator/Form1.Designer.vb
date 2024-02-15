060bfce0e7b57c63fb6048e72bd918ec982c14fd
IhsFace <ihsaanifzal75@gmail.com>
2024-02-15 20:57:33 +1100

    Update files

---
+++ BMI Calculator/BMI Calculator/Form1.Designer.vb
@@ -0,0 +1,109 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        txtHeight = New TextBox()
+        txtWeight = New TextBox()
+        btnCalculate = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 16F, FontStyle.Bold)
+        Label1.Location = New Point(235, 52)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(324, 59)
+        Label1.TabIndex = 0
+        Label1.Text = "BMI Calculator"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(235, 167)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(86, 32)
+        Label2.TabIndex = 1
+        Label2.Text = "Height"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(231, 237)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(90, 32)
+        Label3.TabIndex = 2
+        Label3.Text = "Weight"
+        ' 
+        ' txtHeight
+        ' 
+        txtHeight.Location = New Point(404, 167)
+        txtHeight.Name = "txtHeight"
+        txtHeight.Size = New Size(200, 39)
+        txtHeight.TabIndex = 3
+        ' 
+        ' txtWeight
+        ' 
+        txtWeight.Location = New Point(404, 234)
+        txtWeight.Name = "txtWeight"
+        txtWeight.Size = New Size(200, 39)
+        txtWeight.TabIndex = 4
+        ' 
+        ' btnCalculate
+        ' 
+        btnCalculate.Location = New Point(284, 333)
+        btnCalculate.Name = "btnCalculate"
+        btnCalculate.Size = New Size(225, 46)
+        btnCalculate.TabIndex = 5
+        btnCalculate.Text = "Calculate BMI"
+        btnCalculate.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(13F, 32F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.PaleTurquoise
+        ClientSize = New Size(800, 450)
+        Controls.Add(btnCalculate)
+        Controls.Add(txtWeight)
+        Controls.Add(txtHeight)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtHeight As TextBox
+    Friend WithEvents txtWeight As TextBox
+    Friend WithEvents btnCalculate As Button
+
+End Class
---
+++ BMI Calculator/BMI Calculator/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        Dim fltHeight As Decimal = Val(txtHeight.Text)
+        Dim fltWeight As Decimal = Val(txtWeight.Text)
+
+        If fltHeight <= 0 OrElse fltWeight <= 0 Then
+            MsgBox("Height and weight must be greater than zero")
+            txtHeight.Focus()
+            Return
+        End If
+
+        Dim fltBmi As Decimal = fltWeight / (fltHeight) ^ 2
+
+        Select Case fltBmi
+            Case Is < 18.5
+                MsgBox("Underweight (< 18.5)")
+            Case 18.5 To 24.9
+                MsgBox("Normal weight (18.5 - 24.9)")
+            Case 25 To 29.9
+                MsgBox("Overweight (25 - 29.9)")
+            Case Is >= 30
+                MsgBox("Obese (>= 30)")
+            Case Else
+                MsgBox("There was an error when determining your BMI")
+        End Select
+    End Sub
+End Class
